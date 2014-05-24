d6662973de9b072ddae4883fbd448219ea0fd4ca
deverseli800 <cesar.devers@gmail.com>
2014-05-24 14:30:29 -0400

    alternate css and background image

---
+++ public/javascripts/homeScript.js
@@ -0,0 +1,24 @@
+$(document).ready( function() {
+	//smooth scroll
+		$(function() {
+		  $('a[href*=#]:not([href=#])').click(function() {
+		    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+		      var target = $(this.hash);
+		      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+		      if (target.length) {
+		        $('html,body').animate({
+		          scrollTop: target.offset().top
+		        }, 1000);
+		        return false;
+		      }
+		    }
+		  });
+		});
+
+	//end smooth scroll
+	var windowHeight = $(window).height();
+	var landingHeight = $('.landing-page').height();
+
+	$('.hero-text').css('margin-top', -landingHeight/2);
+	$('.landing-page').css('height', windowHeight);
+})
--- routes/index.js
+++ routes/index.js
@@ -6,3 +6,7 @@
 exports.index = function(req, res){
   res.render('index', { title: 'Express' });
 };
+
+exports.alternate = function(req, res){
+  res.render('alternate', { title: 'Alternate' });
+};
